2242e041d9be88ee449b5c26de454a627f6c4fad
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-10-14 20:56:47 +0100

    creates Model to store interfaces

---
+++ src/Model.ts
@@ -0,0 +1,12 @@
+export interface Chain{
+    chain: string;
+}
+
+export interface Hotel{
+    id: number;
+    name: string;
+    city: string;
+    country: string;
+    address: string;
+    rank: number;
+}
