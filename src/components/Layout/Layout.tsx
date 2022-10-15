00c804014d87aca2afef1481bc99f4627e5b66a8
Kaosi Ezealigo <ezealigokosiso@gmail.com>
2022-10-15 14:00:30 +0100

    minor changes and maps out hotel object down to Hotels Component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,20 +1,26 @@
-import React, { useState } from 'react';
-import { Chain } from '../../Model';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Chain, Hotel } from '../../Model';
+import Hotels from '../Hotel/Hotel';
 import './Layout.css';
 
 interface Props {
-    chain: Chain[];
+    list: Chain[];
     deleteChain: (id: number) => void;
-    editChain: (id: number, text: string) => void
+    editChain: (id: number, text: string) => void;
+    hotel: Hotel[];
+    handleDelete: (id: number) => void;
+    handleEdit: (id: number, name: string, city: string, address: string, country: string, rank: number) => void;
 }
 
 const Layout: React.FC<Props> = (props) => {
-    const [test, setTest] = useState<boolean>(false)
-    const { chain, deleteChain, editChain } = props
+    const { list, deleteChain, editChain, hotel, handleDelete, handleEdit } = props;
+    const navigate = useNavigate();
     return (
         <><section className='layout'>
-            {chain.length
-                ? <div>{chain.map((layout) => {
+            {list.length
+                ? <div>{list.map((layout) => {
+
                     return <>
                         <div className="chainLayout" key={layout.id}>
                             <div className="layoutTop" >
@@ -23,17 +29,19 @@ const Layout: React.FC<Props> = (props) => {
                                 <div className="btns">
                                     <button type='button' className='btn' onClick={() => editChain(layout.id, layout.chain)}>Edit {layout.chain}</button>
                                     <button type='button' className='btn' onClick={() => deleteChain(layout.id)}>Delete {layout.chain}</button>
-                                    <button type='button' className='btn'>Add Hotel</button>
+                                    <button type='button' className='btn' onClick={() => navigate("/addHotel")}>Add Hotel</button>
                                 </div>
 
                             </div>
                             <div className="hotels">
-                                {test ? "" : <span>Add a hotel to chain</span>}
+                                {hotel.length ? hotel.map((item) => {
+                                    return <Hotels key={item.id} item={item} handleDelete={handleDelete} handleEdit={handleEdit} />
+                                }) : <span>Add a hotel to chain</span>}
                             </div>
                         </div>
                     </>
                 })}</div>
-                : <p>No chains avaiable</p>}
+                : <p className='noChains'>No chains avaiable</p>}
         </section></>
     )
 }
